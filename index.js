f286d1c039381ef37c1763b183045e3aeaff8f4a
Sean Hamilton <stillaverage@gmail.com>
2024-07-03 17:49:53 -0700

    feat: filemetadata solution

--- index.js
+++ index.js
@@ -1,17 +1,26 @@
-var express = require('express');
-var cors = require('cors');
+const express = require('express');
+const cors = require('cors');
 require('dotenv').config()
 
-var app = express();
+const app = express();
+
+const multer = require('multer');
+
+const upload = multer({ dest: process.cwd() + '/uploads' })
 
 app.use(cors());
+app.use(express.urlencoded({ extended: true }))
+app.use(express.json())
+
 app.use('/public', express.static(process.cwd() + '/public'));
 
 app.get('/', function (req, res) {
   res.sendFile(process.cwd() + '/views/index.html');
 });
 
-
+app.post('/api/fileanalyse', upload.single('upfile'), (req, res) => {
+  res.json({ name: req.file.originalname, type: req.file.mimetype, size: req.file.size })
+});
 
 
 const port = process.env.PORT || 3000;
